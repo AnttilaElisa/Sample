90173d59424eb1303e7ab9a7b9de798e998e8b2b
Elisa Anttila <elsu.anttila@gmail.com>
2025-09-30 15:03:39 +0300

    toivo elää

--- Week3/TaskA3_T5.py
+++ Week3/TaskA3_T5.py
@@ -1,28 +1,28 @@
-#Program starting.
 print("Program starting.\n")
-#Options:
-#1 - Celsius to Fahrenheit
-#2 - Fahrenheit to Celsius
-#0 - Exit
+
 print("Options:")
-print("#1 - Celcius to Fahrenheit")
-print("#2 - Fahrenheit to Celcius")
-print("#3 - Exit")
-Choise = int(input("Your choise: "))
-#Your choice: 1
-#Insert the amount of Celsius: 23
-#23.0 °C equals to 73.4 °F
-#Program ending.
-if(Choise == 1):
-    Cels = float(input("Insert the amount of Celcius: "))
-    Fahr = Cels * 1.8 + 32
-    print(f"{round(Cels, 1)}°C equals to {round(Fahr, 1)}°F\n")
-elif(Choise == 2):
-    Fahr2 = float(input("Insert the amount of Fahrenheit: "))
-    Cels2 = (Fahr2 - 32) / 1.8
-    print(f"{round(Fahr2, 1)}°F equals to {round(Cels2, 1)}°C\n")
-elif(Choise == 3):
-    print("Exiting...\n")
+print("1 - Celsius to Fahrenheit")
+print("2 - Fahrenheit to Celsius")
+print("0 - Exit")
+
+choice = int(input("Your choice: "))
+
+if(choice == 1):
+    Celsius = float(input("Insert the amount of Celsius: "))
+    Fahrenheits = Celsius * 1.8 + 32
+    print(f"{round(Celsius, 1)}°C equals to {round(Fahrenheits, 1)}°F")
+    print("\nProgram ending.")
+
+elif(choice == 2):
+    Fahrenheits = float(input("Insert the amount of Fahrenheit: "))
+    Celsius = (Fahrenheits - 32) / 1.8
+    print(f"{round(Fahrenheits, 1)}°F equals to {round(Celsius, 1)}°C")
+    print("\nProgram ending.")
+
+elif(choice == 0):
+    print("Exiting...")
+    print("\nProgram ending.")
+
 else:
-    print("Unknown option.\n")
-print("Program ending.")
+    print("Unknown option.")
+    print("\nProgram ending.")
