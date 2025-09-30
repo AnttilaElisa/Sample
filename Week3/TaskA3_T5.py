8571e03f74584b487a857b8b542eeb1dccd1b2a8
Elisa Anttila <elsu.anttila@gmail.com>
2025-09-30 10:41:43 +0300

    jos nyt

--- Week3/TaskA3_T5.py
+++ Week3/TaskA3_T5.py
@@ -1,23 +1,3 @@
-#Start the program by listing options for the user:
-
-#Celsius to Fahrenheit
-#Fahrenheit to Celsius
-#Exit
-#Prompt user to insert choice. After the decision to convert, ask the amount of current temperature (use the floating point datatype). Lastly show the converted value to the user.
-
-#For the unit conversions, use the formula Celsius = (Fahrenheit - 32) / 1.8
-
-#Data representation examples:
-
-#50.0 °F
-#10.0 °C
-#If the user chooses option Exit, notify the user: Exiting...
-
-#Use 1 decimal precision to round the converted value.
-
-#Example program runs
-
-#run 1 run 2 run 3
 #Program starting.
 print("Program starting.\n")
 #Options:
@@ -43,4 +23,6 @@ elif(Choise == 2):
     print(f"{round(Fahr2, 1)}°F equals to {round(Cels2, 1)}°C\n")
 elif(Choise == 3):
     print("Exiting...\n")
+else:
+    print("Unknown option.\n")
 print("Program ending.")
