8a330b7da90b390e9398fdf25df917eb30859a60
Elisa Anttila <elsu.anttila@gmail.com>
2025-10-22 13:16:33 +0300

    h

---
+++ Week6/TaskA6_T1.py
@@ -0,0 +1,22 @@
+#Datasets
+#A6_T1_D1.txt
+#A6_T1_D2.txt
+#A6_T1_D3.txt
+#Create a program that can read a text file and then print the file content. User must be able to specify the file name. Decorate the beginning and the end of the file with a decorative line.
+
+#Decorative lines
+
+# #### START "{filename}" ####
+# #### END "{filename}" ####
+#Example program runs
+
+#run 1
+#Program starting.
+#This program can read a file.
+#Insert filename: A6_T1_D1.txt
+# #### START "A6_T1_D1.txt" ####
+#Hello
+#World
+
+# #### END "A6_T1_D1.txt" ####
+#Program ending.
---
+++ Week6/TaskA6_T2.py
@@ -0,0 +1,10 @@
+#Create a program that can write a text file. Prompt the user to insert first name and last name. Then prompt the file name for the write operation. Finally write a text file with first name on the first row and last name on the second row. Ensure that the text file ends in a empty row. Finally exit the program cleanly.
+
+#Example program runs
+
+#run 1
+#Program starting.
+#Insert first name: Guido
+#Insert last name: Rossum
+#Insert filename: A6_T2_F1.txt
+#Program ending.
